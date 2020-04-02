2809716398e68a7c1331abb38056ddd5b458a461
Matthew Cecil <mcecil@protonmail.com>
2020-04-01 20:48:43 -0400

    reorganized commands into cogs folder

--- run.py
+++ run.py
@@ -4,6 +4,11 @@ import configparser
 import os
 import sys
 import random
+from cogs import *
+
+startup_extensions = [
+    'bot_commands',
+    'bot_events']
 
 config = configparser.ConfigParser()
 config.read(os.environ['D_BOT_CFG'] + 'main.ini')
@@ -12,54 +17,19 @@ token = config['discord']['token']
 
 client = commands.Bot(command_prefix = '.')
 
-@client.event
-async def on_ready():
-    print('Bot is ready')
-
-@client.event
-async def on_member_join(member):
-    print(f'{member} has joined the server.')
-
-@client.event
-async def on_member_remove(member):
-    print(f'{member} has left the server.')
-
 @client.command()
-async def ping(ctx):
-    await ctx.send(f'pong {round(client.latency * 1000)} ms')
+async def load(ctx, extension_name):
+    try:
+        client.load_extension('cogs.' + extension_name)
+    except Exception as e:
+        await client.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
 
-@client.command()
-async def clear(ctx, amount=5):
-    await ctx.channel.purge(limit=amount)
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            client.load_extension('cogs.' + extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
 
-@client.command()
-async def setlastfm(ctx, account_name):
-    print(ctx.author)
-    print(account_name)
-    #await ctx.send('LastFm set!')
-
-@client.command(aliases=['8ball'])
-async def _8ball(ctx, *, question):
-    responses = ["As I see it, yes.",
-        "Ask again later.",
-        "Better not tell you now.",
-        "Cannot predict now.",
-        "Concentrate and ask again.",
-        "Don’t count on it.",
-        "It is certain.",
-        "It is decidedly so.",
-        "Most likely.",
-        "My reply is no.",
-        "My sources say no.",
-        "Outlook not so good.",
-        "Outlook good.",
-        "Reply hazy, try again.",
-        "Signs point to yes.",
-        "Very doubtful.",
-        "Without a doubt.",
-        "Yes.",
-        "Yes – definitely.",
-        "You may rely on it."]
-    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
-    
 client.run(token)
