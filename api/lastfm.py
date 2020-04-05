41b2021ccb369c6033b34f253cb55513f50e8e08
Matthew Cecil <mcecil@protonmail.com>
2020-04-05 18:20:42 -0400

    fixed command for formatting api query string to accommodate params

--- api/lastfm.py
+++ api/lastfm.py
@@ -16,6 +16,28 @@ class Lastfm:
         self.lastfm_key = self.config['lastfm']['api_key']
         self.lastfm_secret = self.config['lastfm']['shared_secret']
         self.url_base = 'http://ws.audioscrobbler.com/2.0/'
+    
+    def format_query_string(self, lastfm_object, method, user, *params):
+        self.query_string = '?method={lastfm_object}.{method}&user={user}&api_key={key}&format=json'.format(
+            lastfm_object=lastfm_object,
+            method=method,
+            user=user,
+            key=self.lastfm_key
+        )
+
+        if params:
+            final_query = ''
+            for param in params:
+                final_query += param
+
+            return self.url_base + self.query_string + final_query
+        else:
+            query = self.url_base + self.query_string
+            return query
 
     def get_recent_tracks(self, user):
         pass
+
+fm = Lastfm()
+q_s = fm.format_query_string('user', 'getrecenttracks', 'Spacesh1p', '&limit=5', '&test=1')
+print(q_s)
